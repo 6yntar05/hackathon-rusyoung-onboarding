c67ccd6a273c0479cf0798adf1b8709c790160a2
6yntar05 <6yntar05@mail.ru>
2023-02-17 20:34:55 +0700

    Initial commit

---
+++ bot/bot.py
@@ -0,0 +1,57 @@
+#!/bin/env python3
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+from utils.token import token_get
+API_TOKEN = token_get()
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    kb = [
+        [
+            types.KeyboardButton(text="Кнопка"),
+            types.KeyboardButton(text="Ещё кнопка")
+        ],
+        [
+            types.KeyboardButton(text="Большая кнопка"),
+        ]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    await message.answer("STARTPAGE", reply_markup=keyboard)
+
+# INLINE TEST
+inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+@dp.message_handler(commands=['1'])
+async def process_command_1(message: types.Message):
+    await message.reply("Первая инлайн кнопка", reply_markup=inline_kb1)
+
+@dp.callback_query_handler(text="button1")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Aboba")
+    await callback.answer(
+        text="Спасибо, что воспользовались ботом!",
+        show_alert=True
+    )
+    # или просто await call.answer()
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    with open('data/cats.jpg', 'rb') as photo:
+        await message.reply_photo(photo, caption='Cats are here 😺')
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ bot/utils/token.py
@@ -0,0 +1,44 @@
+import os
+
+middles = ["token"]
+prefixes = ["", "."]
+postfixes = ["", ".txt", ".json"]
+case_sensivity = False
+
+def possible_names() -> list:
+    """
+    Generate list of possible tokenfile names
+    """
+    _names = []
+
+    for _middle in middles:
+        for _prefix in prefixes:
+            for _postfix in postfixes:
+                _names.append(_prefix + _middle + _postfix)
+    
+    return _names
+
+def token_validate(text:str) -> bool:
+    """
+    Validate token. Considers lenght only for now
+    """
+    if len(text) != 46:
+        return False
+    return True
+
+def token_get() -> str:
+    """
+    Read the token from file (check `possible_names()`)
+    """
+    _names = possible_names()
+    ls = os.listdir()
+    for filename in ls:
+        if filename in _names:
+            _file = open(filename, "r")
+            _text = _file.read()
+            _token = _text.replace("\n", "").replace("{", "").replace("}", "").replace(" ", "")
+            if not token_validate(_token):
+                raise ValueError("Uncorrect token!")
+            return _token
+    
+    raise NameError("Unnable to find tokenfile. Write the token in .token.txt file, for example")
