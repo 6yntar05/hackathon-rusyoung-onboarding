4410085aadf3fbd11e9837a39d67b3dac103c80c
Sodx1 <anthonyprorok@hotmail.com>
2023-02-18 13:45:12 +0700

    Work

--- bot/bot.py
+++ bot/bot.py
@@ -31,30 +31,11 @@ async def send_welcome(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
     await message.answer("Приветствую 👋, ты используешь бота Росмолодежи, выбери себе роль!", reply_markup=keyboard)
 
-# INLINE TEST
-inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
-inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
-@dp.message_handler(commands=['1'])
-async def process_command_1(message: types.Message):
-    await message.reply("Первая инлайн кнопка", reply_markup=inline_kb1)
 
-@dp.callback_query_handler(text="button1")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Aboba")
-    await callback.answer(
-        text="Спасибо, что воспользовались ботом!",
-        show_alert=True
-    )
-    # или просто await call.answer()
 
-@dp.message_handler(regexp='(^cat[s]?$|puss)')
-async def cats(message: types.Message):
-    with open('data/cats.jpg', 'rb') as photo:
-        await message.reply_photo(photo, caption='Cats are here 😺')
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+#@dp.message_handler()
+#async def echo(message: types.Message):
+    #await message.answer(message.text)
 
 if __name__ == '__main__':
     dbacc = sys.argv[1]
