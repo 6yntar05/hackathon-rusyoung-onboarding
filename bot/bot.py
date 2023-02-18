d1054dcec2b04d535120fd93f18e80f992e7e399
Sodx1 <anthonyprorok@hotmail.com>
2023-02-18 15:07:38 +0700

    Work Database

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,8 @@ logging.basicConfig(level=logging.INFO)
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+import aiogram.utils.markdown as fmt
+from aiogram.dispatcher import FSMContext
 
 from utils.token import token_get
 API_TOKEN = token_get()
@@ -16,26 +18,23 @@ API_TOKEN = token_get()
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+#Start Message
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    kb = [
-        [
-            types.KeyboardButton(text="Стажер"),
-            types.KeyboardButton(text="HR"),
-            types.KeyboardButton(text="Ментор")
-        ]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Стажер","HR","Руководитель"]
+    keyboard.add(*buttons)
     await message.answer("Приветствую 👋, ты используешь бота Росмолодежи, выбери себе роль!", reply_markup=keyboard)
 
 
 
-#@dp.message_handler()
-#async def echo(message: types.Message):
-    #await message.answer(message.text)
+@dp.message_handler(regexp='(^Стажер)')
+async def youngcmd_start(message: types.Message):
+    await message.answer('Отлично! Привет, мой друг давай раскажи себе о себе напиши в сообщении /create')
+    #await create_profile(user_id=message.from_user.id)
 
 if __name__ == '__main__':
     dbacc = sys.argv[1]
--- bot/database.py
+++ bot/database.py
@@ -1,3 +1,2 @@
-import psycopg2
 
-conn = psycopg2.connect(dbname = '', user='', password='',host='',port='')
+
